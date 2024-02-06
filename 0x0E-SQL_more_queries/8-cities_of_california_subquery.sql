646cb44a538b68ebd5ff641003b5adcfe0bb7eba
Reward Mucheche <89034732+manhimself26@users.noreply.github.com>
2024-02-06 07:19:45 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,8 +0,0 @@
--- liists all the cities of California that can be found in the database
-SELECT `id`, `name`
-  FROM `cities`
- WHERE `state_id` IN
-       (SELECT `id`
-	        FROM `states`
-	       WHERE `name` = "California")
-ORDER BY `id`;
